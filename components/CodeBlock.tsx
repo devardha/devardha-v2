56eb483e79771e0951705422f5b1b9a1edf575f6
dev-ardha <ardhaydhtm@gmail.com>
2021-02-14 00:07:15 +0700

    Update codeblock

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,13 +1,13 @@
 import React, { PureComponent } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { duotoneSea } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { nord } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 class CodeBlock extends PureComponent {
     render() {
-        const { language, value }: any = this.props;
+        const { language, value,  }: any = this.props;
 
         return (
-            <SyntaxHighlighter language={language} style={duotoneSea}>
+            <SyntaxHighlighter language={language} style={nord} showLineNumbers={true}>
                 {value}
             </SyntaxHighlighter>
         );
