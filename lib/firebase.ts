d43d113a9bb763754acdcf67244a86c2799afd83
dev-ardha <ardhaydhtm@gmail.com>
2021-02-15 19:50:39 +0700

    Fix firbase credentials error

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,7 @@ import 'firebase/analytics'
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
